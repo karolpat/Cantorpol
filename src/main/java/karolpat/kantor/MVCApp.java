b518638bef83f4ce7fedd4aafa1072e9624bb6e6
Patecki <karol.patecki@accenture.com>
2018-10-04 09:57:07 +0200

    My first frame

---
+++ src/main/java/karolpat/kantor/MVCApp.java
@@ -0,0 +1,16 @@
+package karolpat.kantor;
+
+import javax.swing.JFrame;
+
+public class MVCApp extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	public MVCApp(){
+		super("First frame");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100,100,400,270);
+	}
+	
+
+}
